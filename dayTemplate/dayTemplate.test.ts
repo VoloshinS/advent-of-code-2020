87a1c933f6e07a8966d3d0518b8e898f00b0eee5
Serhii Voloshyn <serhii.voloshyn@alteryx.com>
2020-12-07 13:56:38 +0200

    Initial commit

---
+++ dayTemplate/dayTemplate.test.ts
@@ -0,0 +1,20 @@
+const { test, expect } = require("@jest/globals");
+
+import { func1, func2 } from './dayTemplate';
+import { testInput, input } from './dayTemplate.input';
+
+describe('dayTemplate', () => {
+  describe('func1', () => {
+    test('should work as expected', () => {
+      expect(func1(testInput)).toBe(undefined);
+      expect(func1(input)).toBe(undefined);
+    });
+  });
+
+  describe('func2', () => {
+    test('should work as expected', () => {
+      expect(func2(testInput)).toBe(undefined);
+      expect(func2(input)).toBe(undefined);
+    });
+  });
+});
---
+++ dayTemplate/dayTemplate.ts
@@ -0,0 +1,15 @@
+const dataPreparator = (str: string) => {
+  return ;
+};
+
+// Puzzle 1
+export const func1 = (input: string) => {
+  const prepared = dataPreparator(input);
+  return;
+}
+
+// Puzzle 2
+export const func2 = (input: string) => {
+  const prepared = dataPreparator(input);
+  return;
+}
